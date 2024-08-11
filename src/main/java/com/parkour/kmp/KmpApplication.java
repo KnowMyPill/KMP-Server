bdb78cc8af68f02050bbaf5c613635cc134acca7
becooq81 <becooq81@gmail.com>
2024-08-12 03:06:13 +0900

    Initial commit

---
+++ src/main/java/com/parkour/kmp/KmpApplication.java
@@ -0,0 +1,13 @@
+package com.parkour.kmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkour/kmp/KmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkour.kmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
