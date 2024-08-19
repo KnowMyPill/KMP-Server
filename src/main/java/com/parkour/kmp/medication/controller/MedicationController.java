2a6d0a507fd968df94d47a84762f44122b5fd51e
becooq81 <becooq81@gmail.com>
2024-08-19 17:49:44 +0900

    feat: set up Medication controller

--- src/main/java/com/parkour/kmp/medication/controller/MedicationController.java
+++ src/main/java/com/parkour/kmp/medication/controller/MedicationController.java
@@ -1,11 +1,21 @@
 package com.parkour.kmp.medication.controller;
 
+import com.parkour.kmp.medication.payload.response.MedicationResponse;
 import com.parkour.kmp.medication.service.MedicationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/medication")
 @RequiredArgsConstructor
 public class MedicationController {
     private final MedicationService medicationService;
+
+    @GetMapping
+    public ResponseEntity<MedicationResponse> searchMedicationByBarcode(@PathVariable String barcode) {
+        return ResponseEntity.status(HttpStatus.OK).body(medicationService.findMedicationByBarcode(barcode));
+    }
 }
