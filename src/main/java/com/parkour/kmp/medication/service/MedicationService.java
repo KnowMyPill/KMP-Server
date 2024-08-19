07023b33612f38d69cfa5121b549cf5b4bc1ecd0
becooq81 <becooq81@gmail.com>
2024-08-19 17:50:57 +0900

    feat(Medication): set up Medication service

--- src/main/java/com/parkour/kmp/medication/service/MedicationService.java
+++ src/main/java/com/parkour/kmp/medication/service/MedicationService.java
@@ -1,7 +1,22 @@
 package com.parkour.kmp.medication.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.parkour.kmp.api.MedicationApiInvoker;
+import com.parkour.kmp.medication.domain.Medication;
+import com.parkour.kmp.medication.payload.response.MedicationApiResponse;
+import com.parkour.kmp.medication.payload.response.MedicationResponse;
+import com.parkour.kmp.medication.repository.MedicationRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MedicationService {
+    private final MedicationRepository medicationRepository;
+    private final MedicationApiInvoker apiInvoker;
+
+    public MedicationResponse findMedicationByBarcode(String barcode) {
+        // TODO: 바코드 값을 의약품표준코드로 맵핑한다
+        MedicationApiResponse medicationApiResponse = apiInvoker.fetchMedicationData(medicationCode);
+    }
 }
