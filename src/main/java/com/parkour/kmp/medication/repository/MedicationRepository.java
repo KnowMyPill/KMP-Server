5a1ca129daf06b6189087374e539b5ecacaa1ff8
becooq81 <becooq81@gmail.com>
2024-08-19 17:50:43 +0900

    feat(Medication): set up jpa repository interface

--- src/main/java/com/parkour/kmp/medication/repository/MedicationRepository.java
+++ src/main/java/com/parkour/kmp/medication/repository/MedicationRepository.java
@@ -1,2 +1,9 @@
-package com.parkour.kmp.medication.repository;public interface MedicationRepository {
+package com.parkour.kmp.medication.repository;
+
+import com.parkour.kmp.medication.domain.Medication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
 }
