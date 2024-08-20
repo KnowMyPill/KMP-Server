36510676ff06f354cc424840d094ab1dab663060
becooq81 <becooq81@gmail.com>
2024-08-20 10:15:34 +0900

    feat(api): add api invoker factory to implement factory pattern in api invokers - use factory pattern to prevent redundant initializations of fields: rest template, object mapper, etc. - createInvoker method accepts invoker cmd enum

--- src/main/java/com/parkour/kmp/api/invoker/MedicationApiInvokerFactory.java
+++ src/main/java/com/parkour/kmp/api/invoker/MedicationApiInvokerFactory.java
@@ -1,2 +1,23 @@
-package com.parkour.kmp.api.invoker;public class MedicationApiInvokerFactory {
+package com.parkour.kmp.api.invoker;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.parkour.kmp.api.domain.MedicationApiInvokerCommand;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class MedicationApiInvokerFactory {
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    public MedicationApiInvokerFactory(RestTemplateBuilder restTemplateBuilder, ObjectMapper objectMapper) {
+        this.restTemplate = restTemplateBuilder.build();
+        this.objectMapper = objectMapper;
+    }
+
+    public MedicationApiInvoker createMedicationApiInvoker(MedicationApiInvokerCommand command) {
+        return new MedicationApiInvoker(restTemplate, objectMapper, command);
+    }
+
 }
