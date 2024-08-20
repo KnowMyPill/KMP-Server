e2dfc405725db828869e02bc35f8b7ba92fa3db7
becooq81 <becooq81@gmail.com>
2024-08-20 10:17:41 +0900

    feat(api): add api call method to fetch all data of med code

--- src/main/java/com/parkour/kmp/api/invoker/MedicationApiInvoker.java
+++ src/main/java/com/parkour/kmp/api/invoker/MedicationApiInvoker.java
@@ -2,28 +2,83 @@ package com.parkour.kmp.api.invoker;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.parkour.kmp.api.domain.MedicationApiInvokerCommand;
 import com.parkour.kmp.api.exception.InvalidRequestException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-import com.parkour.kmp.medication.payload.response.MedicationApiResponse;
+import com.parkour.kmp.api.payload.response.MedCodeApiResponse;
+import com.parkour.kmp.api.payload.response.MedCodeSummaryResponse;
+import com.parkour.kmp.api.payload.response.MedicationApiResponse;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@Component
 public class MedicationApiInvoker {
 
     private static final String apiKey = System.getenv("KMP_APP_APIKEY");
-    private static final String url = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/";
 
-    private final RestTemplate restTemplate = new RestTemplate();
-    private final ObjectMapper mapper = new ObjectMapper();
+    private final RestTemplate restTemplate;
+    private final ObjectMapper mapper;
 
-    private HttpMethod method = HttpMethod.GET;
-    private String path = "getDrbEasyDrugList";
+    private final String url, path;
+    private final HttpMethod method;
+
+    public MedicationApiInvoker(RestTemplate restTemplate, ObjectMapper mapper, MedicationApiInvokerCommand command) {
+        this.restTemplate = restTemplate;
+        this.mapper = mapper;
+        this.url = command.getUrl();
+        this.path = command.getPath();
+        this.method = command.getMethod();
+    }
+
+    public MedCodeSummaryResponse fetchAllMedCodeData() throws InvalidRequestException {
+        List<MedCodeApiResponse> responses = new ArrayList<>();
+        int currentPage = 1;
+        int pageSize = 100;
+
+        while (true) {
+            URI uri = UriComponentsBuilder.fromHttpUrl(url)
+                    .path(path)
+                    .queryParam("serviceKey", apiKey)
+                    .queryParam("page", currentPage)
+                    .queryParam("perPage", pageSize)
+                    .encode()
+                    .build()
+                    .toUri();
+
+            RequestEntity<Void> requestEntity = buildRequest(method, uri).build();
+            ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
+
+            if (!responseEntity.getStatusCode().is2xxSuccessful()) {
+                throw new InvalidRequestException("Failed to fetch med code data: " + responseEntity.getBody());
+            }
+
+            String responseBody = responseEntity.getBody();
+            try {
+
+                Map<String, Object> responseMap = mapper.readValue(responseBody, Map.class);
+                List<Map<String, Object>> data = (List<Map<String, Object>>) responseMap.get("data");
+
+                for (Map<String, Object> itemMap : data) {
+                    MedCodeApiResponse item = mapper.convertValue(itemMap, MedCodeApiResponse.class);
+                    responses.add(item);
+                }
+
+                if (data.size() < pageSize) {
+                    break;
+                }
+                currentPage++;
+            } catch (JsonProcessingException e) {
+                throw new InvalidRequestException("Error processing JSON response: " + e);
+            }
+        }
+        return new MedCodeSummaryResponse(pageSize, responses.size(), responses.size(), responses);
+    }
 
     public MedicationApiResponse fetchMedicationData(String query) throws InvalidRequestException, JsonProcessingException {
         URI uri = UriComponentsBuilder.fromHttpUrl(url)
