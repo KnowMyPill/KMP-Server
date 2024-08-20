566794007c714a01b6a9ca5890cbb124507c1c58
becooq81 <becooq81@gmail.com>
2024-08-20 10:13:51 +0900

    feat(ApiCmd): add method as field for cmd for expandability

--- src/main/java/com/parkour/kmp/api/domain/MedicationApiInvokerCommand.java
+++ src/main/java/com/parkour/kmp/api/domain/MedicationApiInvokerCommand.java
@@ -1,15 +1,19 @@
 package com.parkour.kmp.api.domain;
 
 import lombok.Getter;
+import org.springframework.http.HttpMethod;
 
 @Getter
 public enum MedicationApiInvokerCommand {
-    GET_MED_FROM_CODE("getDrbEasyDrugList");
-    // TODO: 코드 맵핑 데이터 어떻게 가져올지
+    GET_MED_FROM_CODE("http://apis.data.go.kr/1471000/DrbEasyDrugInfoService", "/getDrbEasyDrugList", HttpMethod.GET),
+    GET_CODE_FROM_BARCODE("api.odcloud.kr/api", "/15067462/v1/uddi:4c72e98f-1bf9-470f-bee0-f451d54cd871", HttpMethod.GET);
 
-    private final String url;
+    private final String url, path;
+    private final HttpMethod method;
 
-    MedicationApiInvokerCommand(String url) {
+    MedicationApiInvokerCommand(String url, String path, HttpMethod method) {
         this.url = url;
+        this.path = path;
+        this.method = method;
     }
 }
