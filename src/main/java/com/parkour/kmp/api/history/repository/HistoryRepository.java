bb1a7d63c1e72f6336c31f4a99a1cc4b8a865320
becooq81 <becooq81@gmail.com>
2024-10-16 23:19:01 +0900

    feat: add method to find all histories

--- src/main/java/com/parkour/kmp/api/history/repository/HistoryRepository.java
+++ src/main/java/com/parkour/kmp/api/history/repository/HistoryRepository.java
@@ -1,2 +1,18 @@
-package com.parkour.kmp.api.history.repository;public interface HistoryRepository {
+package com.parkour.kmp.api.history.repository;
+
+import com.parkour.kmp.api.history.domain.History;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HistoryRepository extends JpaRepository<History, Long>, PagingAndSortingRepository<History, Long> {
+    @Query("SELECT h FROM History h JOIN FETCH h.user u JOIN FETCH h.medication m WHERE u.id = :userId")
+    Page<History> findAllByUser(@Param("userId") Long userId, Pageable pageable);
 }
