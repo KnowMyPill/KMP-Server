7d92787d9f8663f1d4e54eaafaed43c81579e723
becooq81 <becooq81@gmail.com>
2024-10-17 01:02:30 +0900

    fix: add repository annotation

--- src/main/java/com/parkour/kmp/api/user/repository/UserRepository.java
+++ src/main/java/com/parkour/kmp/api/user/repository/UserRepository.java
@@ -4,8 +4,9 @@ import com.parkour.kmp.api.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByToken(String token);
-
 }
