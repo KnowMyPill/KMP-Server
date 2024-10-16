bad58076e2b3e6f2e25983c6efbee20c201e1b2f
becooq81 <becooq81@gmail.com>
2024-10-16 17:58:26 +0900

    feat: implement database initializer for medcodes

--- src/main/java/com/parkour/kmp/api/batch/loader/DataLoader.java
+++ src/main/java/com/parkour/kmp/api/batch/loader/DataLoader.java
@@ -1,4 +1,37 @@
 package com.parkour.kmp.api.batch.loader;
 
-public class DataLoader {
+import com.opencsv.CSVReader;
+import com.parkour.kmp.api.medcode.domain.MedCode;
+import com.parkour.kmp.api.medcode.repository.MedCodeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+@Component
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+
+    private final MedCodeRepository medCodeRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        Resource resource = new ClassPathResource("/data/MEDCODE_20231030.csv");
+
+        try (CSVReader csvReader = new CSVReader(new InputStreamReader(resource.getInputStream()))) {
+            String[] nextLine;
+            csvReader.readNext();
+            while ((nextLine = csvReader.readNext()) != null) {
+                MedCode medcode = new MedCode(
+                        nextLine[6], nextLine[9], nextLine[10], nextLine[11]
+                );
+                medCodeRepository.save(medcode);
+            }
+        }
+    }
 }
