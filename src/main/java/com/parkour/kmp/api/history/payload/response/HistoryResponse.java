30599ca05d03a100d60f80468778d9b68e3130d0
becooq81 <becooq81@gmail.com>
2024-10-16 23:18:50 +0900

    feat: add history response

--- src/main/java/com/parkour/kmp/api/history/payload/response/HistoryResponse.java
+++ src/main/java/com/parkour/kmp/api/history/payload/response/HistoryResponse.java
@@ -1,2 +1,17 @@
-package com.parkour.kmp.api.history.payload.response;public class HistoryResponse {
+package com.parkour.kmp.api.history.payload.response;
+
+import com.parkour.kmp.api.medication.payload.response.MedicationResponse;
+import lombok.Getter;
+
+@Getter
+public class HistoryResponse {
+    String userId;
+    MedicationResponse medication;
+
+    protected HistoryResponse() {}
+
+    public HistoryResponse(String userId, MedicationResponse medication) {
+        this.userId = userId;
+        this.medication = medication;
+    }
 }
