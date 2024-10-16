11ebc2617c8f6282841cbeaa7c692bca046fde22
becooq81 <becooq81@gmail.com>
2024-10-17 01:02:02 +0900

    feat: add signup for user controller

--- src/main/java/com/parkour/kmp/api/user/controller/UserController.java
+++ src/main/java/com/parkour/kmp/api/user/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.parkour.kmp.api.user.controller;
 
+import com.parkour.kmp.api.user.payload.request.UserSignUpRequest;
+import com.parkour.kmp.api.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody UserSignUpRequest request) {
+        userService.signUp(request);
+        return ResponseEntity.ok().build();
+    }
 }
