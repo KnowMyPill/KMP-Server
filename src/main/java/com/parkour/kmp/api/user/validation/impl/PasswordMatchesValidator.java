e26f511fd467b9fb756aff0023498b04a6f9f708
becooq81 <becooq81@gmail.com>
2024-10-16 17:35:27 +0900

    feat: add validators for unique email and matching passwords

--- src/main/java/com/parkour/kmp/api/user/validation/impl/PasswordMatchesValidator.java
+++ src/main/java/com/parkour/kmp/api/user/validation/impl/PasswordMatchesValidator.java
@@ -1,4 +1,15 @@
 package com.parkour.kmp.api.user.validation.impl;
 
-public class PasswordMatchesValidator {
+import com.parkour.kmp.api.user.payload.request.UserSignUpRequest;
+import com.parkour.kmp.api.user.validation.PasswordMatches;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, UserSignUpRequest> {
+
+    @Override
+    public boolean isValid(UserSignUpRequest userSignUpDto, ConstraintValidatorContext context) {
+        return userSignUpDto.password() != null
+                && userSignUpDto.password().equals(userSignUpDto.checkPassword());
+    }
 }
--- src/main/java/com/parkour/kmp/api/user/validation/impl/UniqueEmailValidator.java
+++ src/main/java/com/parkour/kmp/api/user/validation/impl/UniqueEmailValidator.java
@@ -1,4 +1,22 @@
 package com.parkour.kmp.api.user.validation.impl;
 
-public class UniqueEmailValidator {
+import com.parkour.kmp.api.user.repository.UserRepository;
+import com.parkour.kmp.api.user.validation.UniqueEmail;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UniqueEmailValidator(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        return email != null && userRepository.findByEmail(email).isEmpty();
+    }
 }
