75cd771fd731341110d5b735c8e96c7d9d361359
becooq81 <becooq81@gmail.com>
2024-10-16 17:35:03 +0900

    feat: add user repository

--- src/main/java/com/parkour/kmp/api/user/repository/UserRepository.java
+++ src/main/java/com/parkour/kmp/api/user/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.parkour.kmp.api.user.repository;
 
-public interface UserRepository {
+import com.parkour.kmp.api.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
 }
