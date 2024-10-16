5fc88b0ecae373eceb3e0b38c7383ed27dee6719
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-10-16 16:42:36 +0900

    feat: rename fields to avoid mysql reserved keywords ("desc")

--- src/main/java/com/parkour/kmp/api/medication/domain/Medication.java
+++ src/main/java/com/parkour/kmp/api/medication/domain/Medication.java
@@ -21,31 +21,31 @@ public class Medication extends TimestampEntity {
     private String itemName;
 
     // 설명 1. 효능: 이 약의 효능은 무엇입니까?
-    @Column(name = "desc_effect")
+    @Column(name = "description_effect")
     private String descEffect;
 
     // 설명 2. 사용법: 이 약은 어떻게 사용합니까?
-    @Column(name = "desc_use_method")
+    @Column(name = "description_use_method")
     private String descUseMethod;
 
     // 설명 3. 주의사항 경고: 이 약을 사용하기 전에 반드시 알아야 할 내용은 무엇입니까?
-    @Column(name = "desc_warning_before_usage")
+    @Column(name = "description_warning_before_usage")
     private String descWarningBeforeUsage;
 
     // 설명 4. 주의사항: 이 약의 사용상 주의사항은 무엇입니까?
-    @Column(name = "desc_warning")
+    @Column(name = "description_warning")
     private String descWarning;
 
     // 설명 5. 상호작용: 이 약을 사용하는 동안 주의해야 할 약 또는 음식은 무엇입니까?
-    @Column(name = "desc_banned_combo")
+    @Column(name = "description_banned_combo")
     private String descBannedCombo;
 
     // 설명 6. 부작용: 이 약은 어떤 이상반응이 나타날 수 있습니까?
-    @Column(name = "desc_side_effect")
+    @Column(name = "description_side_effect")
     private String descSideEffect;
 
     // 설명 7. 보관법: 이 약은 어떻게 보관해야 합니까?
-    @Column(name = "desc_manage_method")
+    @Column(name = "description_manage_method")
     private String descManageMethod;
 
     protected Medication() {}
