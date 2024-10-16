b1e03e21869847709ed461a112e802849e120f30
becooq81 <becooq81@gmail.com>
2024-10-16 17:17:41 +0900

    fix: add imports

--- src/main/java/com/parkour/kmp/api/medication/controller/MedicationController.java
+++ src/main/java/com/parkour/kmp/api/medication/controller/MedicationController.java
@@ -5,9 +5,7 @@ import com.parkour.kmp.api.medication.service.MedicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/medication")
 @RequiredArgsConstructor
