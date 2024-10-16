cc24316ba71e3b6c8e5ec60b71f6eb68fad4280c
becooq81 <becooq81@gmail.com>
2024-10-17 01:01:22 +0900

    feat: add api to url

--- src/main/java/com/parkour/kmp/api/medication/controller/MedicationController.java
+++ src/main/java/com/parkour/kmp/api/medication/controller/MedicationController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/medication")
+@RequestMapping("/api/medication")
 @RequiredArgsConstructor
 @RestController
 public class MedicationController {
