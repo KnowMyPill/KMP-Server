f41415d1e14bd439a0e4111e369e1e115081630e
becooq81 <becooq81@gmail.com>
2024-10-16 23:21:23 +0900

    feat: set up history controller

--- src/main/java/com/parkour/kmp/api/history/controller/HistoryController.java
+++ src/main/java/com/parkour/kmp/api/history/controller/HistoryController.java
@@ -1,2 +1,19 @@
-package com.parkour.kmp.api.history.controller;public class HistoryController {
+package com.parkour.kmp.api.history.controller;
+
+import com.parkour.kmp.api.history.service.HistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/history")
+@RequiredArgsConstructor
+public class HistoryController {
+    private final HistoryService historyService;
+
+    @GetMapping("/my")
+    public void getMyHistory() {
+
+    }
 }
