39688d5892b44edc5017903d92f77bdb72f9c59f
becooq81 <becooq81@gmail.com>
2024-10-17 01:02:18 +0900

    feat: remove validation

--- src/main/java/com/parkour/kmp/api/user/payload/request/UserSignUpRequest.java
+++ src/main/java/com/parkour/kmp/api/user/payload/request/UserSignUpRequest.java
@@ -1,9 +1,3 @@
 package com.parkour.kmp.api.user.payload.request;
 
-import com.parkour.kmp.api.user.validation.PasswordMatches;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-@PasswordMatches
 public record UserSignUpRequest(String token) {}
