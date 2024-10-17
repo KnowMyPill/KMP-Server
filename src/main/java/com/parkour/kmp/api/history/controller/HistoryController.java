bd2d3c6f2126c6752711ea5c6dc3eaddfe380060
becooq81 <becooq81@gmail.com>
2024-10-17 13:41:01 +0900

    feat: change response type to dto

--- src/main/java/com/parkour/kmp/api/history/controller/HistoryController.java
+++ src/main/java/com/parkour/kmp/api/history/controller/HistoryController.java
@@ -2,18 +2,14 @@ package com.parkour.kmp.api.history.controller;
 
 import com.parkour.kmp.api.history.domain.History;
 import com.parkour.kmp.api.history.payload.request.HistoryStoreRequest;
+import com.parkour.kmp.api.history.payload.response.HistoryResponse;
 import com.parkour.kmp.api.history.service.HistoryService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/history")
@@ -22,13 +18,13 @@ public class HistoryController {
     private final HistoryService historyService;
 
     @GetMapping("/my")
-    public ResponseEntity<List<History>> getMyHistory(@RequestParam String token,
-                                                      @RequestParam(defaultValue = "0") int page) {
-        Page<History> histories = historyService.findHistoriesByUser(token, page);
+    public ResponseEntity<List<HistoryResponse>> getMyHistory(@RequestParam String token,
+                                                              @RequestParam(defaultValue = "0") int page) {
+        List<HistoryResponse> histories = historyService.findHistoriesByUser(token, page);
         if (histories.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
-        return ResponseEntity.ok().body(histories.getContent());
+        return ResponseEntity.ok().body(histories);
     }
 
     @PostMapping("/store")
@@ -39,6 +35,5 @@ public class HistoryController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
-
     }
 }
