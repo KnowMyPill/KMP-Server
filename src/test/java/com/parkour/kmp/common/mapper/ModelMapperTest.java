84c1e71da4a99553e282d67e16eda869f4e7dd68
becooq81 <becooq81@gmail.com>
2024-10-17 13:45:13 +0900

    feat: add model mapper test

--- src/test/java/com/parkour/kmp/common/mapper/ModelMapperTest.java
+++ src/test/java/com/parkour/kmp/common/mapper/ModelMapperTest.java
@@ -1,4 +1,32 @@
 package com.parkour.kmp.common.mapper;
+import com.parkour.kmp.api.medication.domain.Medication;
+import com.parkour.kmp.api.medication.payload.response.MedicationResponse;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ModelMapperTest {
+
+    private final ModelMapper modelMapper = new ModelMapper();
+
+    @Test
+    public void testMappingMedicationResponseToMedication() {
+        MedicationResponse response = new MedicationResponse(
+                "itemSeq123",
+                "Test Medication",
+                "Helps with headaches",
+                "Take one pill daily",
+                "Avoid alcohol",
+                "No known warnings",
+                "Not applicable",
+                "Nausea",
+                "Store in a cool place"
+        );
+
+        Medication medication = modelMapper.map(response, Medication.class);
+
+        assertEquals("itemSeq123", medication.getItemSeq());
+    }
 }
+
