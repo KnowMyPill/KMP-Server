3f91bee69d83169846df1d54c0e73c282ab2ffe5
becooq81 <becooq81@gmail.com>
2024-10-17 13:45:02 +0900

    feat: set up mapper config for record class

--- src/main/java/com/parkour/kmp/common/config/ModelMapperConfig.java
+++ src/main/java/com/parkour/kmp/common/config/ModelMapperConfig.java
@@ -1,19 +1,26 @@
 package com.parkour.kmp.common.config;
 
+import com.parkour.kmp.api.medication.domain.Medication;
+import com.parkour.kmp.api.medication.payload.response.MedicationResponse;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.config.Configuration.AccessLevel;
+import org.modelmapper.record.RecordModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Configuration
 public class ModelMapperConfig {
+    private static final Logger logger = LoggerFactory.getLogger(ModelMapperConfig.class);
 
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper().registerModule(new RecordModule());
         modelMapper.getConfiguration()
                 .setFieldAccessLevel(AccessLevel.PRIVATE)
                 .setFieldMatchingEnabled(true);
+
         return modelMapper;
     }
 
