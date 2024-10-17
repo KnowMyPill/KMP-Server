b05ea197feff90bf063c8687af6bddf05d8c18dd
becooq81 <becooq81@gmail.com>
2024-10-17 10:30:30 +0900

    refactor: remove unused imports

--- src/test/java/com/parkour/kmp/api/history/service/HistoryServiceImplTest.java
+++ src/test/java/com/parkour/kmp/api/history/service/HistoryServiceImplTest.java
@@ -14,10 +14,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class HistoryServiceImplTest {
