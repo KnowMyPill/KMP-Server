897012163f0f53ee56da52a988f49a206af3a95d
becooq81 <becooq81@gmail.com>
2024-10-17 13:41:26 +0900

    refactor: remove unused imports

--- src/main/java/com/parkour/kmp/api/history/repository/HistoryRepository.java
+++ src/main/java/com/parkour/kmp/api/history/repository/HistoryRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +12,5 @@ import org.springframework.stereotype.Repository;
 public interface HistoryRepository extends JpaRepository<History, Long> {
     @Query("SELECT h FROM History h JOIN FETCH h.user u JOIN FETCH h.medication m WHERE u.token = :token")
     Page<History> findAllByUser(@Param("token") String token, Pageable pageable);
+
 }
