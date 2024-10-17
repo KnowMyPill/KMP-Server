67782bb3cfce8b9256da230ddaee051538fcc5f4
becooq81 <becooq81@gmail.com>
2024-10-17 10:30:55 +0900

    feat: add user controller/service test

--- src/test/java/com/parkour/kmp/api/user/controller/UserControllerTest.java
+++ src/test/java/com/parkour/kmp/api/user/controller/UserControllerTest.java
@@ -3,12 +3,17 @@ package com.parkour.kmp.api.user.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.parkour.kmp.api.user.domain.User;
+import com.parkour.kmp.api.user.payload.request.UserSignUpRequest;
+import com.parkour.kmp.api.user.repository.UserRepository;
 import com.parkour.kmp.api.user.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(UserController.class)
@@ -17,14 +22,35 @@ public class UserControllerTest {
     @Autowired
     MockMvc mvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @MockBean
     UserService userService;
+
+    @MockBean
+    UserRepository userRepository;
 
     @Test
     @DisplayName("사용자 회원가입 테스트")
     void createUserTest() throws Exception{
-        String token = "1234567890";
-        mvc.perform(post("/api/user"))
+        UserSignUpRequest request = new UserSignUpRequest("123482");
+        mvc.perform(post("/api/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                )
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("중복 토큰으로 회원가입 실패 테스트")
+    void createUser_shouldFailWithDuplicateToken() throws Exception {
+        userRepository.save(new User("123482"));
+        UserSignUpRequest request = new UserSignUpRequest("123482");
+
+        mvc.perform(post("/api/user/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        ).andExpect(status().is4xxClientError());
     }
 }
--- src/test/java/com/parkour/kmp/api/user/service/UserServiceImplTest.java
+++ src/test/java/com/parkour/kmp/api/user/service/UserServiceImplTest.java
@@ -1,4 +1,63 @@
 package com.parkour.kmp.api.user.service;
 
+import com.parkour.kmp.api.user.domain.User;
+import com.parkour.kmp.api.user.payload.request.UserSignUpRequest;
+import com.parkour.kmp.api.user.repository.UserRepository;
+import com.parkour.kmp.api.user.service.impl.UserServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void signUp_success() {
+        UserSignUpRequest request = new UserSignUpRequest("1234");
+        User user = new User("1234");
+        when(userRepository.save(any(User.class))).thenReturn(user);
+        User savedUser = userService.signUp(request);
+
+        verify(userRepository, times(1)).save(any(User.class));
+        assertNotNull(savedUser);
+        assertEquals("1234", savedUser.getToken());
+    }
+
+    @Test
+    @DisplayName("회원 식별 성공")
+    void findUserByToken_success() {
+        String token = "5678";
+        User user = new User(token);
+        when(userRepository.findByToken(token)).thenReturn(Optional.of(user));
+
+        User found = userService.findUserByToken(token);
+        assertNotNull(found);
+        assertEquals(token, found.getToken());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 회원 식별 실패")
+    void findUserByToken_fail() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class,
+                () -> userService.findUserByToken("0000"));
+        assertEquals(thrown.getMessage(), "User not found");
+
+    }
 }
