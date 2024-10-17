e9f9fc290f6972c4d00748620560ca56ee840bd1
becooq81 <becooq81@gmail.com>
2024-10-17 13:44:28 +0900

    feat: add methods to identify existence or entity

--- src/main/java/com/parkour/kmp/api/medication/repository/MedicationRepository.java
+++ src/main/java/com/parkour/kmp/api/medication/repository/MedicationRepository.java
@@ -4,6 +4,10 @@ import com.parkour.kmp.api.medication.domain.Medication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MedicationRepository extends JpaRepository<Medication, Long> {
+    boolean existsByItemSeq(String itemSeq);
+    Optional<Medication> findByItemSeq(String itemSeq);
 }
--- src/main/java/com/parkour/kmp/api/medication/service/impl/MedicationServiceImpl.java
+++ src/main/java/com/parkour/kmp/api/medication/service/impl/MedicationServiceImpl.java
@@ -1,9 +1,10 @@
 package com.parkour.kmp.api.medication.service.impl;
 
-import com.parkour.kmp.api.client.domain.ApiInvokerCmd;
+import com.parkour.kmp.api.client.exception.InvalidRequestException;
 import com.parkour.kmp.api.client.invoker.MedDataApiInvoker;
 import com.parkour.kmp.api.medcode.domain.MedCode;
 import com.parkour.kmp.api.medcode.service.MedCodeService;
+import com.parkour.kmp.api.medication.domain.Medication;
 import com.parkour.kmp.api.medication.payload.response.MedicationResponse;
 import com.parkour.kmp.api.medication.repository.MedicationRepository;
 import com.parkour.kmp.api.medication.service.MedicationService;
@@ -14,22 +15,56 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MedicationServiceImpl implements MedicationService {
-    private final MedicationRepository medicationRepository;
     private final MedCodeService medCodeService;
     private final MedDataApiInvoker invoker;
+    private final MedicationRepository medicationRepository;
 
     @Override
-    public MedicationResponse findMedicationByBarcode(String barcode) {
+    public MedicationResponse findMedicationByBarcode(String barcode) throws InvalidRequestException {
+        String processedCode = processCode(barcode);
+        MedCode medCode = medCodeService.findMedCodeByCodeStandard(processedCode);
 
-        MedItemApiResponse medItemApiResponse = null;
+        MedItemApiResponse medItemApiResponse = fetchMedicationData(medCode);
+
+        return buildMedicationResponse(medItemApiResponse);
+    }
+
+    @Override
+    public Medication saveMedication(Medication medication){
+        return medicationRepository.save(medication);
+    }
+
+    @Override
+    public boolean existsByItemSeq(String itemSeq) {
+        return medicationRepository.existsByItemSeq(itemSeq);
+    }
+
+    @Override
+    public Medication retrieveMedicationByItemSeq(String itemSeq) throws InvalidRequestException {
+        return medicationRepository.findByItemSeq(itemSeq)
+                .orElseThrow(() -> new InvalidRequestException("Medication does not exist for given item sequence."));
+    }
+
+    @Override
+    public MedicationResponse findMedicationByItemSeq(String itemSeq) {
+        MedItemApiResponse item = invoker.fetchMedicationData(itemSeq);
+        return buildMedicationResponse(item);
+    }
+
+    private MedItemApiResponse fetchMedicationData(MedCode medCode) {
         try {
-            String code = processCode(barcode);
-            MedCode medcode = medCodeService.findMedCodeByCodeStandard(code);
-            medItemApiResponse = invoker.fetchMedicationData(medcode.getItemSeq());
+            return invoker.fetchMedicationData(medCode.getItemSeq());
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new IllegalArgumentException("Medication does not exist in e약은요 database.", e);
+        }
+    }
+
+    private MedicationResponse buildMedicationResponse(MedItemApiResponse medItemApiResponse) {
+        if (medItemApiResponse == null) {
+            throw new IllegalArgumentException("Medication does not exist for given item sequence");
         }
         return new MedicationResponse(
+                medItemApiResponse.getItemSeq(),
                 medItemApiResponse.getItemName(),
                 medItemApiResponse.getEfcyQesitm(),
                 medItemApiResponse.getUseMethodQesitm(),
@@ -41,26 +76,8 @@ public class MedicationServiceImpl implements MedicationService {
         );
     }
 
-    @Override
-    public MedicationResponse findMedicationByItemSeq(String itemSeq) {
-        MedItemApiResponse item = invoker.fetchMedicationData(itemSeq);
-        return new MedicationResponse(
-                item.getItemName(),
-                item.getEfcyQesitm(),
-                item.getUseMethodQesitm(),
-                item.getAtpnWarnQesitm(),
-                item.getAtpnQesitm(),
-                item.getIntrcQesitm(),
-                item.getSeQesitm(),
-                item.getDepositMethodQesitm()
-        );
-    }
-
     private String processCode(String barcode) {
         barcode = barcode.trim();
-        if (barcode.startsWith("88")) {
-            return barcode.substring(1);
-        }
-        return barcode;
+        return barcode.length() > 13 ? barcode.substring(1) : barcode;
     }
 }
