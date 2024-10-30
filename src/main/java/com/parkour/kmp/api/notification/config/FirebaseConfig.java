850737b1763b80030f4a06434c2460a3f5f3f001
becooq81 <becooq81@gmail.com>
2024-10-30 20:18:07 +0900

    feat: add firebase config beans

---
+++ src/main/java/com/parkour/kmp/api/notification/config/FirebaseConfig.java
@@ -0,0 +1,27 @@
+package com.parkour.kmp.api.notification.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FirebaseConfig {
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FileInputStream firebaseJson = new FileInputStream("src/main/resources/fcm-private-key.json");
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(firebaseJson))
+                .build();
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging() {
+        return FirebaseMessaging.getInstance();
+    }
+}
