d816724a730ab207689a73c93d9e037acf7077ae
becooq81 <becooq81@gmail.com>
2024-10-31 14:40:25 +0900

    feat: replace hard coded value for private key file name

--- src/main/java/com/parkour/kmp/api/fcm/configuration/FirebaseConfig.java
+++ src/main/java/com/parkour/kmp/api/fcm/configuration/FirebaseConfig.java
@@ -6,14 +6,20 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import java.io.FileInputStream;
 import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FirebaseConfig {
+
+    @Value("${fcm.private-key}")
+    private String FCM_PRIVATE_KEY;
+
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream firebaseJson = new FileInputStream("src/main/resources/fcm-private-key.json");
+        FileInputStream firebaseJson = new FileInputStream("src/main/resources/"+FCM_PRIVATE_KEY);
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(firebaseJson))
                 .build();
